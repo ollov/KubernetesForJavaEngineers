f69574dcceb959c87973f310cfb8b2910ec5affe
Aleh Samoilau <Aleh.Samoilau@McKesson.com>
2023-05-16 20:35:38 +0200

    lesson2: use volumeClaimTemplates for DB StatefulSets

--- manifest/delete.cmd
+++ manifest/delete.cmd
@@ -2,6 +2,6 @@ kubectl delete -f manifest_users-service.yml
 kubectl delete -f manifest_posts-service.yml
 kubectl delete -f manifest_posts-db.yml
 kubectl delete -f manifest_users-db.yml
-kubectl delete -f manifest_ns.yml
+@rem kubectl delete -f manifest_ns.yml
 
 kubectl get all -n=k8s-program
