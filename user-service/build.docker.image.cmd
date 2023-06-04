a62fae2c32248a7b397ac3f18d883dc6850047af
Aleh Samoilau <Aleh.Samoilau@McKesson.com>
2023-06-04 23:57:53 +0200

    lesson3: Sub-task 2: Liveness and Readiness probes
    Add endpoints for health checks at your applications.
    Add startup, liveness and readiness probes for your Deployment objects at k8s manifest.
    Add startup, liveness and readiness probes for your StatefulSet objects at k8s manifest.

--- user-service/build.docker.image.cmd
+++ user-service/build.docker.image.cmd
@@ -1,4 +1,4 @@
-@rem mvnw clean package -Dspring.profiles.active=local -Dmaven.test.skip=true -f pom.xml
+@rem mvnw clean package
 
 docker build -t ooo4u/user-service:1.0.0 ./
 docker push ooo4u/user-service:1.0.0
